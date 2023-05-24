714db98fe78d2b9e76bc0452ed3e2a6a5ee08d7b
TeraNovaLP <16338782+TeraNovaLP@users.noreply.github.com>
2023-05-24 22:07:27 +0200

    Add layout component

--- resources/views/shortener.blade.php
+++ resources/views/shortener.blade.php
@@ -1,11 +1,12 @@
-<h1>URL Shortener</h1>
-<form action="/shortener/" method="post">
-    @csrf
-    <label>URL</label>
-    <input type="url" class="form-control" name="url" id="url" />
-    <button type="submit" class="btn btn-primary">Submit!</button>
-</form>
-@if (!empty($id))
-<label>ShortURL:</label>
-<a href="http://localhost/shortener/{{$id}}" target="_blank">http://localhost/shortener/{{$id}}</a>
-@endif
+<x-layout title="URL Shortener">
+    <form action="/shortener/" method="post">
+        @csrf
+        <label>URL</label>
+        <input type="url" class="form-control" name="url" id="url" />
+        <button type="submit" class="btn btn-primary">Submit!</button>
+    </form>
+    @if (!empty($id))
+    <label>ShortURL:</label>
+    <a href="http://localhost/shortener/{{$id}}" target="_blank">http://localhost/shortener/{{$id}}</a>
+    @endif
+</x-layout>
--- resources/views/todo.blade.php
+++ resources/views/todo.blade.php
@@ -1,17 +1,18 @@
-<h1>ToDo</h1>
-<form action="/todo/new" method="post">
-    @csrf
-    <label>Name</label>
-    <input type="text" class="form-control" name="name" id="name" />
-    <button type="submit">Submit!</button>
-</form>
-@foreach ($items as $item)
-<form action="/todo/{{$item->id}}/update" method="post">
-    @csrf
-    <label>Name</label>
-    <input type="text" class="form-control" name="name" id="name" value="{{$item->name}}" />
-    <label>Done</label>
-    <input type="checkbox" class="form-control" name="done" id="done" @if ($item->done) checked @endif />
-    <button type="submit">Submit!</button>
-</form>
-@endforeach
+<x-layout title="ToDo">
+    <form action="/todo/new" method="post">
+        @csrf
+        <label>Name</label>
+        <input type="text" class="form-control" name="name" id="name" />
+        <button type="submit">Submit!</button>
+    </form>
+    @foreach ($items as $item)
+    <form action="/todo/{{$item->id}}/update" method="post">
+        @csrf
+        <label>Name</label>
+        <input type="text" class="form-control" name="name" id="name" value="{{$item->name}}" />
+        <label>Done</label>
+        <input type="checkbox" class="form-control" name="done" id="done" @if ($item->done) checked @endif />
+        <button type="submit">Submit!</button>
+    </form>
+    @endforeach
+</x-layout>
